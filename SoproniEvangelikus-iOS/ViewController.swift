6ff84ec03e013474c9d50ab7b831346dde6ddf8e
admin <admin@20014c4e1149510018361f1dda0c2266.dsl.pool.telekom.hu>
2019-12-13 13:42:25 +0100

    Initial Commit

---
+++ SoproniEvangelikus-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoproniEvangelikus-iOS
+//
+//  Created by admin on 2019. 12. 13..
+//  Copyright © 2019. admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
